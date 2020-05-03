3a43c3c988dbcdd9a9226e111f9612a176701033
Mauri Niininen <mauri@innomore.com>
2020-05-03 07:40:18 -0400

    Training using create_image2() - use spectrogram to form training images. Tested training and it converged

--- MorseDecoder.py
+++ MorseDecoder.py
@@ -671,7 +671,7 @@ class MorseDataset():
         "iterator"
         batchRange = range(self.currIdx, self.currIdx + self.batchSize)
         gtTexts = [self.samples[i].gtText for i in batchRange]
-        imgs = [create_image(self.samples[i].filePath, self.imgSize, self.dataAugmentation) for i in batchRange]
+        imgs = [create_image2(self.samples[i].filePath, self.imgSize, self.dataAugmentation) for i in batchRange]
         #imgs = [preprocess(cv2.imread(self.samples[i].filePath, cv2.IMREAD_GRAYSCALE), self.imgSize, self.dataAugmentation) for i in batchRange]
         self.currIdx += self.batchSize
         return Batch(gtTexts, imgs)
